1aac01e396b2dd20c591f396f2876ad71c466535
Sergio Lopez <sslopezrocket@gmail.com>
2020-07-07 19:30:35 -0700

    Trying to add a datetime selector but the ts seems to be acting up

--- src/app/add-tasks/add-tasks.page.ts
+++ src/app/add-tasks/add-tasks.page.ts
@@ -6,8 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-tasks.page.scss'],
 })
 export class AddTasksPage implements OnInit {
+  myDate = new Date().toISOString();
+  customPickerOptions: any;
+  customYearValues = [2020, 2016, 2008, 2004, 2000, 1996];
+  customDayShortNames = ['s\u00f8n', 'man', 'tir', 'ons', 'tor', 'fre', 'l\u00f8r'];
 
-  constructor() { }
+  constructor() { 
+    this.customPickerOptions = {
+      buttons: [{
+        text: 'Save',
+        handler: () => console.log('Clicked Save!')
+      }, {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+          return false;
+        }
+      }]
+    }
+  }
+
+  dateChanged(date) {
+    console.log(date.detail.value);
+    console.log(this.myDate);
+  }
+}
+  }
 
   ngOnInit() {
   }
