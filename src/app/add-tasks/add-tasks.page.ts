f62829713e577d31af9465e803a5082f4c93feb3
Sergio Lopez <sslopezrocket@gmail.com>
2020-07-07 22:02:35 -0700

    Use Chrome from now on

--- src/app/add-tasks/add-tasks.page.ts
+++ src/app/add-tasks/add-tasks.page.ts
@@ -5,35 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add-tasks.page.html',
   styleUrls: ['./add-tasks.page.scss'],
 })
-export class AddTasksPage implements OnInit {
-  myDate = new Date().toISOString();
-  customPickerOptions: any;
-  customYearValues = [2020, 2016, 2008, 2004, 2000, 1996];
-  customDayShortNames = ['s\u00f8n', 'man', 'tir', 'ons', 'tor', 'fre', 'l\u00f8r'];
-
-  constructor() { 
-    this.customPickerOptions = {
-      buttons: [{
-        text: 'Save',
-        handler: () => console.log('Clicked Save!')
-      }, {
-        text: 'Log',
-        handler: () => {
-          console.log('Clicked Log. Do not Dismiss.');
-          return false;
-        }
-      }]
-    }
+export class AddTasksPage  {
+  
+  constructor() {
   }
-
-  dateChanged(date) {
-    console.log(date.detail.value);
-    console.log(this.myDate);
-  }
-}
-  }
-
-  ngOnInit() {
-  }
-
-}
+  } 
